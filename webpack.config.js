6a286d4f32a0aac48514456d0d01dd80e8286ca5
Anders Bredgaard Thuesen <andersbthuesen@gmail.com>
2018-01-20 17:44:41 +0100

    Added webpack build system

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+const commonConfig = require("./webpack.common");
+const merge = require("webpack-merge");
+
+const config = merge(commonConfig, {
+  devServer: {
+    port: 3000,
+    contentBase: path.join(__dirname, "dist")
+  }
+});
+module.exports = config;
