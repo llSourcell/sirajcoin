fb3313ec0d0f421bcf0c24fe60a857a21e1037eb
Anders Bredgaard Thuesen <andersbthuesen@gmail.com>
2018-01-26 15:05:33 +0100

    Added ipc wallet actions

--- wallet/index.js
+++ wallet/index.js
@@ -1,6 +1,9 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
 const url = require("url");
+const Credentials = require("../lib/Credentials");
+const Wallet = require("../lib/Wallet");
+const { getClient } = require("../lib/client");
 
 // This class is responsible for setting up the GUI wallet using electron.
 class SirajcoinWallet {
@@ -9,6 +12,37 @@ class SirajcoinWallet {
     this.handleClose = this.handleClose.bind(this);
     this.handleReady = this.handleReady.bind(this);
     this.handleActivate = this.handleActivate.bind(this);
+    this.wallet$ = Promise.all([getClient(), Credentials.fromFile()]).then(
+      ([client, credentials]) => {
+        return new Wallet(client, credentials);
+      }
+    );
+
+    ipcMain.on("getBalance", async event => {
+      const wallet = await this.wallet$;
+      const balance = await wallet.getBalance();
+      event.sender.send("balance", balance);
+    });
+
+    ipcMain.on("getAddress", async event => {
+      const wallet = await this.wallet$;
+      const address = await wallet.getAddress();
+      event.sender.send("address", address);
+    });
+
+    ipcMain.on("send", async (event, address, amount) => {
+      const wallet = await this.wallet$;
+      try {
+        //await wallet.send(address, amount);
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        event.sender.send("send", true);
+      } catch (e) {
+        event.sender.send("send", false);
+      }
+      //console.log(arg1, arg2);
+      // TODO send sirajcoin
+      //const wallet = await this.wallet$;
+    });
   }
 
   handleClose() {
@@ -39,7 +73,7 @@ class SirajcoinWallet {
 
   createWindow() {
     // Create the browser window.
-    const window = new BrowserWindow({ width: 400, height: 600 });
+    const window = new BrowserWindow({ width: 500, height: 400 });
     window.setTitle("Sirajcoin Wallet");
     window.loadURL(
       url.format({
@@ -51,7 +85,7 @@ class SirajcoinWallet {
     //window.loadURL("http://localhost:8080");
 
     if (process.env.NODE_ENV !== "production") {
-      //window.webContents.openDevTools();
+      window.webContents.openDevTools();
     }
 
     return window;
